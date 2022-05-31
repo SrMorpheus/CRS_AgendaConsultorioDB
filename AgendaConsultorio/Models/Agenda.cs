ba8ebbd66b6f81a48a5e5c8bbfdbadce8659d248
Bruno Gonçalves Barros <brunobarroscc@gmail.com>
2022-05-31 11:11:03 -0300

    Models adicionadas

---
+++ AgendaConsultorio/Models/Agenda.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaConsultorio.Models
+{
+    public class Agenda
+    {
+        public long CPF { get; set; }
+
+        public DateTime DataConsulta { get; private set; }
+
+        public DateTime HoraInicial { get; private set; }
+
+        public DateTime HoraFinal { get; private set; }
+
+        public Paciente Paciente { get;  set; }
+
+
+
+
+        public Agenda(long cpf , DateTime dataConsulta , DateTime horaInicial, DateTime horaFinal)
+        {
+            this.CPF = cpf;
+            
+            this.DataConsulta = dataConsulta;
+
+            this.HoraInicial = horaInicial;
+
+            this.HoraFinal = horaFinal;
+
+
+        }
+
+
+
+
+
+    }
+}
---
+++ AgendaConsultorio/Models/Paciente.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgendaConsultorio.Models
+{
+    public class Paciente
+    {
+
+        private string _nome;
+
+        private long _cpf;
+
+        private DateTime _dataNascimento;
+
+        public string Nome
+        {
+            get
+            {
+
+                return _nome;
+
+            }
+
+             private set
+            {
+
+                _nome = value;
+
+            }
+        }
+
+        public long CPF 
+        {
+            
+            get 
+            {
+
+                return _cpf;
+            
+            }
+            set 
+            { 
+
+                _cpf = value;
+            
+            }
+        }
+
+
+        public DateTime DataNascimento { 
+            get 
+            {
+
+                return _dataNascimento;
+            
+            } 
+            set
+            { 
+            
+               _dataNascimento = value;
+
+            
+            }
+        }
+
+
+        public List<Agenda> Agendas { get; set; }   = new List<Agenda> ();
+
+
+        public Paciente (string nome, long cpf, DateTime dataNascimento)
+        {
+           
+            this.Nome = nome;
+
+            this.CPF = cpf;
+
+            this.DataNascimento = dataNascimento;
+
+
+            
+        }
+
+
+    }
+}
