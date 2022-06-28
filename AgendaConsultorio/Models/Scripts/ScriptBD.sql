78f48d1ad4a2fbe97647dbd0764efdc3b054d387
Bruno Gonçalves Barros <brunobarroscc@gmail.com>
2022-06-28 11:13:03 -0300

    script do banco

---
+++ AgendaConsultorio/Models/Scripts/ScriptBD.sql
@@ -0,0 +1,49 @@
+CREATE DATABASE ConsultorioDB;
+
+Go
+
+USE ConsultorioDB
+
+Go
+
+CREATE TABLE Tbl_Paciente(
+
+Cod_Paciente INT NOT NULL IDENTITY PRIMARY KEY,
+
+CPF_Paciente BIGINT NOT NULL ,
+
+Nome_Paciente VARCHAR(90) NOT NULL,
+
+Data_Nascimento DATE NOT NULL,
+
+
+)
+
+GO
+
+
+CREATE TABLE Tbl_Agenda (
+
+Cod_Agenda INT NOT NULL IDENTITY PRIMARY KEY ,
+
+CPF_Paciente_Agenda BIGINT NOT NULL,
+
+Data_Consulta DATE NOT NULL,
+
+Hora_Inicial DATETIME NOT NULL,
+
+Hora_Final DATETIME NOT NULL,
+
+DATA_Hora_Consulta DATETIME NOT NULL,
+
+FK_Paciente INT NOT NULL
+
+
+)
+
+GO
+
+ALTER TABLE Tbl_Agenda 
+ADD CONSTRAINT FK_PACIENTE_ID FOREIGN KEY(FK_Paciente)
+	REFERENCES Tbl_Paciente (Cod_Paciente)
+GO
